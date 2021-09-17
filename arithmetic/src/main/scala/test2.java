84539c060c6b429a0be0da64b6b12ce36b8c1dea
YAMSS-XF <452368321@qq.com>
2021-09-18 03:18:41 +0800

    test2

---
+++ arithmetic/src/main/scala/test2.java
@@ -0,0 +1,10 @@
+/**
+ * @Author xiefeng
+ * @DATA 2021/9/18 3:18
+ * @Version 1.0
+ */
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
