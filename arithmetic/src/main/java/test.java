693721db9781f8396373025ad7aff1cce03ea6ab
YAMSS-XF <452368321@qq.com>
2021-09-18 03:15:50 +0800

    Change FileName

---
+++ arithmetic/src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author xiefeng
+ * @DATA 2021/9/18 3:15
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
